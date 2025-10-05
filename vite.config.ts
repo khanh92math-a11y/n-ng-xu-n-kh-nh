caa56a1fd1565ece189be7b092a00887ca0198b8
khanh92math-a11y <khanh92.math@gmail.com>
2025-10-05 23:19:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-https://khanh92math.github.io/khanh92math-a11y/
+base: "/khanh92math-a11y/",
       server: {
         port: 3000,
         host: '0.0.0.0',
